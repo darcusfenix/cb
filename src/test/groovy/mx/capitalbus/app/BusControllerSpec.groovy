4a8535a1b7cf159fb81b561ae76efb3c6924ea62
Juan Crisóstomo <juancvfenix@gmail.com>
2016-03-18 10:45:24 -0600

    un archivo de testing

---
+++ src/test/groovy/mx/capitalbus/app/BusControllerSpec.groovy
@@ -0,0 +1,22 @@
+package mx.capitalbus.app
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(BusController)
+class BusControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect: "fix me"
+        true == false
+    }
+}
