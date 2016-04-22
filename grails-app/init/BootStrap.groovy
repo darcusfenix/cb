6a765c16cbebc0b5bed72f05894c310991b169a8
Juan Crisóstomo <juancvfenix@gmail.com>
2016-04-22 13:12:52 -0500

    mi despedida

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -19,10 +19,10 @@ import org.springframework.web.context.support.WebApplicationContextUtils
 class BootStrap {
 
     def init = { servletContext ->
-        /*
+       
         def springContext = WebApplicationContextUtils.getWebApplicationContext(servletContext)
         springContext.getBean("customObjectMarshallers").register()
-        */
+
     }
     def destroy = {
     }
